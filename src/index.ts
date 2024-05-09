93ed20136854c69b24d1bf92be749300ac764a5f
lasse2507a <lran18@student.aau.dk>
2024-05-09 11:29:49 +0200

    removed hardcode

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,14 @@ import { Request, Response } from 'express';
 import cors from "cors";
 import * as bodyParser from "body-parser";
 import { InfluxDB, Point } from '@influxdata/influxdb-client';
-import { addData, addPredictions } from '../influx-data-generator/data-generator';
+
 
 dotenv.config()
 const app = express();
 const port = 3000;
 
 app.disable("x-powered-by");
-app.use(bodyParser.json({limit:  '50mb'}));
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cors());
 
 const url: string = process.env.INFLUX_URL!
@@ -19,15 +19,12 @@ const token: string = process.env.INFLUX_TOKEN!
 const org: string = process.env.INFLUX_ORG!
 const bucket: string = process.env.INFLUX_BUCKET!
 
-const queryApi = new InfluxDB({url, token}).getQueryApi(org);
+const queryApi = new InfluxDB({ url, token }).getQueryApi(org);
 
-// Middleware to parse JSON bodies
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-	const writeApi = new InfluxDB({url, token}).getWriteApi(org, bucket);
-	addPredictions(writeApi);
-	addData(writeApi)
+	const writeApi = new InfluxDB({ url, token }).getWriteApi(org, bucket);
 	res.status(200).send('Hello, TypeScript with Express!');
 });
 
@@ -35,7 +32,7 @@ app.get("/", (req: Request, res: Response) => {
 app.get("/api/predictions", async (req: Request, res: Response) => {
 	let o: any;
 
-	queryApi.queryRows('from(bucket: "test") |> range(start: -365d) |> filter(fn: (r) => r._measurement == "prediction")', {
+	queryApi.queryRows(`from(bucket: "${bucket}") |> range(start: -365d) |> filter(fn: (r) => r._measurement == "prediction")`, {
 		next(row, tableMeta) {
 			o = tableMeta.toObject(row);                  // TODO: Check that this is correct
 			console.log(`${o._time} ${o._measurement}: ${o._field}=${o._value}`);
@@ -52,10 +49,10 @@ app.get("/api/predictions", async (req: Request, res: Response) => {
 });
 
 app.post("/api/stress-generator", async (req: Request, res: Response) => {
-	const writeApi = new InfluxDB({url, token}).getWriteApi(org, bucket);
+	const writeApi = new InfluxDB({ url, token }).getWriteApi(org, bucket);
 	let list_of_points: any[] = [];
 
-	if(Array.isArray(req.body))
+	if (Array.isArray(req.body))
 		req.body.forEach(item => {
 			list_of_points.push(
 				new Point('data')
@@ -66,11 +63,11 @@ app.post("/api/stress-generator", async (req: Request, res: Response) => {
 					.intField('value', item.value)
 			);
 		});
-		writeApi.writePoints(list_of_points);
-		writeApi.close().then(() => {
-			console.log('WRITE FINISHED');
-			res.status(200).send();
-		});
+	writeApi.writePoints(list_of_points);
+	writeApi.close().then(() => {
+		console.log('WRITE FINISHED');
+		res.status(200).send();
+	});
 });
 
 // GET request from stress predictor
@@ -78,54 +75,54 @@ app.route("/api/stress-predict")
 	.get(async (req: Request, res: Response) => {
 		let next_window_to_predict: number | null = null;
 
-		queryApi.queryRows(`from(bucket: "test") 
+		queryApi.queryRows(`from(bucket: "${bucket}") 
 		|> range(start: -inf)
 		|> filter(fn: (r) => r["_measurement"] == "prediction") 
 		|> filter(fn: (r) => r["_field"] == "window_id") 
-		|> max()`, 
-		{
-			next(row, tableMeta) {
-				const rowData = tableMeta.toObject(row)
-				console.log(`${rowData._measurement}: ${rowData._field}=${rowData._value}`);
-				next_window_to_predict = rowData._value + 1 ;
-			},
-			error(error) {
-				console.error('Error fetching data from InfluxDB:', error);
-				res.status(500).send('Internal server error');
-			},
-			complete() {
-				console.log(`The window found is: ${next_window_to_predict}`);
+		|> max()`,
+			{
+				next(row, tableMeta) {
+					const rowData = tableMeta.toObject(row)
+					console.log(`${rowData._measurement}: ${rowData._field}=${rowData._value}`);
+					next_window_to_predict = rowData._value + 1;
+				},
+				error(error) {
+					console.error('Error fetching data from InfluxDB:', error);
+					res.status(500).send('Internal server error');
+				},
+				complete() {
+					console.log(`The window found is: ${next_window_to_predict}`);
 
-				if(next_window_to_predict == null) {
-					influxdbQuerier(
-						`from(bucket: "test") 
+					if (next_window_to_predict == null) {
+						influxdbQuerier(
+							`from(bucket: "${bucket}") 
 							|> range(start: -inf)
 							|> filter(fn: (r) => r._measurement == "data" and r._field == "window_id")
-							|> min()`, 
-						next_window_to_predict, 
-						res
-					);
-				} else {
-					influxdbQuerier(
-						`from(bucket: "test") 
+							|> min()`,
+							next_window_to_predict,
+							res
+						);
+					} else {
+						influxdbQuerier(
+							`from(bucket: "${bucket}") 
 							|> range(start: -inf)
 							|> filter(fn: (r) => r._measurement == "data" and 
 								r._field == "window_id" and r._value == ${next_window_to_predict})`,
-						next_window_to_predict,
-						res    
-					);
-				}
-			},
-		});
+							next_window_to_predict,
+							res
+						);
+					}
+				},
+			});
 	})
 	.post(async (req: Request, res: Response) => {
-		const writeApi = new InfluxDB({url, token}).getWriteApi(org, bucket);
+		const writeApi = new InfluxDB({ url, token }).getWriteApi(org, bucket);
 
 		// Checks if the request comes from the stress generator (req.body is of type list of Object) or 
 		// stress predictor (req.body is of type Object).
-		if(Array.isArray(req.body)){
+		if (Array.isArray(req.body)) {
 			let list_of_points: any[] = [];
-			
+
 			req.body.forEach(item => {
 				const milliseconds = new Date(item.time + "Z").getTime();
 				const nanoseconds = milliseconds * 1e6;
@@ -158,24 +155,24 @@ app.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`);
 });
 
-function influxdbQuerier(query: string, next_window_to_predict: number|null, res: Response) {
+function influxdbQuerier(query: string, next_window_to_predict: number | null, res: Response) {
 	let list_of_results: any[] = [];
 	queryApi.queryRows(query,
-	{
-		next(row, tableMeta) {
-			const rowData = tableMeta.toObject(row)
-			console.log(`${rowData._measurement}: ${rowData._field}=${rowData._value}`);
-			list_of_results.push(rowData);
-		},
-		error(error) {
-			console.error('Error fetching data from InfluxDB:', error);
-			res.status(500).send('Internal server error');
-		},
-		complete() {
-			if(next_window_to_predict == null)
-				res.status(404).send("No data available");
-			else
-				res.status(200).json(list_of_results);
-		},
-	});
+		{
+			next(row, tableMeta) {
+				const rowData = tableMeta.toObject(row)
+				console.log(`${rowData._measurement}: ${rowData._field}=${rowData._value}`);
+				list_of_results.push(rowData);
+			},
+			error(error) {
+				console.error('Error fetching data from InfluxDB:', error);
+				res.status(500).send('Internal server error');
+			},
+			complete() {
+				if (next_window_to_predict == null)
+					res.status(404).send("No data available");
+				else
+					res.status(200).json(list_of_results);
+			},
+		});
 }
